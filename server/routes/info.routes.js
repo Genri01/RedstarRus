f82cca9630c73adf09d4cebbfc2bdde4bc3cb77a
webslick <cnbv170291@yandex.ru>
2021-06-09 00:34:17 +0700

    fatality

--- server/routes/info.routes.js
+++ server/routes/info.routes.js
@@ -3,18 +3,16 @@ const sendmail = require('sendmail')();
 const infoRouter = Router()
 
 infoRouter.post('/sendMail',(req,res) => {
-  const { formForgot } = req.body
-  
-  if(formForgot !== undefined) {
+  const { email, msg } = req.body
+  if(email !== undefined && msg !== undefined) {
     sendmail({
-      from: `${formForgot}`,
-      // to: 'webdev170291@yandex.ru',
-      to: 'Spmain54@mail.ru',
-      subject: `Запрос на восстонавление пароля от <${formForgot}>`,
-      html: `Пользователь с ником ${formForgot.bold()} запрашивает пароль`,
+      from: `${email}`,
+      to: 'info@redstarbaltic.com',
+      subject: `Запрос на обратную связь от <${email}>`,
+      html: `Пользователь с ником ${email.bold()} запрашивает обратную связь. Прислал сообщение с текстом: " ${msg} "`,
     }, function(err, reply) {
-      res.status(200).send({msg: "Email succes"})
-      
+      res.status(200).send({msg: "Email success"})
+
     })
   }else {
     res.status(200).send({msg: "Sorry blank page :("})
@@ -23,3 +21,5 @@ infoRouter.post('/sendMail',(req,res) => {
 });
 
 module.exports = infoRouter;
+
+
