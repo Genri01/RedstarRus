7e59c2173940c1b76ee02c136e27be04e01b869f
webslick <cnbv170291@yandex.ru>
2021-08-02 01:17:15 +0700

    centered img

--- src/components/TitleBlock/index.js
+++ src/components/TitleBlock/index.js
@@ -22,9 +22,9 @@ function TitleBlock(props) {
     return (
       <section style={{height: h ? `${h}px`: ''}} className="main_container">
         {/* <div style={{backgroundImage:`url(${img_head})`}} className="main_container_img"> */}
-        <div className={`${product ? 'main_container_img_product' : 'main_container_img'}`}>
-          <img className="main_img" src={img_head}  alt="main_img" width="100%" height="100%" />
-          <div style={{top: ht ? `${ht}px`: ''}} className="img_cap"></div>
+        <div style={{backgroundImage:`url(${img_head})`}}  className={`${product ? 'main_container_img_product' : 'main_container_img'}`}>
+          {/* <img className="main_img" src={img_head}  alt="main_img" width="100%" height="100%" /> */}
+          {/* <div style={{top: ht ? `${ht}px`: ''}} className="img_cap"></div> */}
           <div className="text_container_fix">
             <div className="main_container_title_block_header">{header}</div>
             <div className="main_container_title_block_subheader">{subheader}</div>
@@ -34,7 +34,7 @@ function TitleBlock(props) {
         <div className="main_container_info">
           {/* <div style={{paddingBottom: hb ? `${hb}px`: ''}} className={`${product ? 'main_container_title_block_product':'main_container_title_block'} ${h ? 'none_padding' : ''}`}> */}
           <div style={{paddingBottom: hb ? `${hb}px`: ''}} className={`${product ? 'main_container_title_block_product':'main_container_title_block'} `}>
-            <div className="main_container_title">
+            <div className={`${product ? "main_container_title_product" : "main_container_title"}`}>
               {title}
             </div>
             <div className={`${product ? 'main_container_subtitle_product' : "main_container_subtitle"}`}>
