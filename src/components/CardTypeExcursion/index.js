df655202e0373a2968f77bf2f0ff953f096e0507
Slick <cnbv170291@yandex.ru>
2021-12-30 23:56:30 +0700

    fix optimise boost img

--- src/components/CardTypeExcursion/index.js
+++ src/components/CardTypeExcursion/index.js
@@ -9,7 +9,7 @@ function CardTypeExcursion(props) {
     return (
       <Link to={link}><div id="excursion" className="cards_container">
         <Lazy component="a" href="/" className="image-link image-link--100px" ltIE9>
-            <img className="cards_container_img" src={`https://pixboost.com/api/2/img/https://redstar-rus-genri01.vercel.app/${images}/optimise&auth=MTQ4OTY2NTYyMw__`} alt="cards" width="100%" height="100%" />
+            <img className="cards_container_img" src={`https://pixboost.com/api/2/img/https://redstar-rus-genri01.vercel.app/${images}/optimise?auth=MTQ4OTY2NTYyMw__`} alt="cards" width="100%" height="100%" />
         </Lazy>
         <div className="cards_container_title">{title}</div>
       </div></Link>
