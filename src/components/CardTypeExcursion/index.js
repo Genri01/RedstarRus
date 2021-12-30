e20e365f2b4fbf7da47623aacb8e6b4161a02932
Slick <cnbv170291@yandex.ru>
2021-12-31 00:19:48 +0700

    fix optimise boost img

--- src/components/CardTypeExcursion/index.js
+++ src/components/CardTypeExcursion/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {HashLink as Link} from 'react-router-hash-link'
-import { Lazy } from 'react-lazy'
 import './style.css';
 
 function CardTypeExcursion(props) {
@@ -8,9 +7,7 @@ function CardTypeExcursion(props) {
 
     return (
       <Link to={link}><div id="excursion" className="cards_container">
-        <Lazy component="a" href="/" className="image-link image-link--100px" ltIE9>
-            <img className="cards_container_img" src={`https://pixboost.com/api/2/img/https://redstar-rus-genri01.vercel.app/${images}/optimise?auth=MTQ4OTY2NTYyMw__`} alt="cards" width="100%" height="100%" />
-        </Lazy>
+        <img className="cards_container_img" src={`https://pixboost.com/api/2/img/https://redstar-rus-genri01.vercel.app/${images}/optimise?auth=MTQ4OTY2NTYyMw__`} alt="cards" width="100%" height="100%" />
         <div className="cards_container_title">{title}</div>
       </div></Link>
     );
