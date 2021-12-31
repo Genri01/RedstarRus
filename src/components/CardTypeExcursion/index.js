2312e93d475719328d9caa5ecc6e34a298ea0010
Slick <cnbv170291@yandex.ru>
2021-12-31 19:42:46 +0700

    test

--- src/components/CardTypeExcursion/index.js
+++ src/components/CardTypeExcursion/index.js
@@ -7,7 +7,7 @@ function CardTypeExcursion(props) {
 
     return (
       <Link to={link}><div id="excursion" className="cards_container">
-        <img className="cards_container_img" src={`https://pixboost.com/api/2/img/https://redstar-rus-genri01.vercel.app/${images}/optimise?auth=MTQ4OTY2NTYyMw__`} alt="cards" width="100%" height="100%" />
+        <img className="cards_container_img" src={images} alt="cards" width="100%" height="100%" />
         <div className="cards_container_title">{title}</div>
       </div></Link>
     );
